37d79f00b9b23be1afaa3ac7cd6e4660264dd56f
Jacob Arrington <Jacobsarrington@gmail.com>
2023-11-05 17:19:06 -0500

    reodered theme migration

---
+++ backend/db/migrations/20231105163216-create-themes.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; 
+   // define your schema in options object
+}
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Themes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      themeName: {
+        type: Sequelize.STRING
+      },
+      themeCategory: {
+        type: Sequelize.STRING
+      },
+      primaryColor: {
+        type: Sequelize.STRING
+      },
+      secondaryColor: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Themes";
+    return queryInterface.dropTable(options);
+  }
+};
