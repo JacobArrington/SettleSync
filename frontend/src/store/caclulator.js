a88f308eeecc1b3fa85dc954716ffcadac1d1eb4
Jacob Arrington <Jacobsarrington@gmail.com>
2023-11-26 18:20:27 -0500

    crud action creators made

--- frontend/src/store/caclulator.js
+++ frontend/src/store/caclulator.js
@@ -2,3 +2,24 @@ const GET_CALC = "calculator/GET_CALC"
 const ADD_CALC = "calculator/ADD_CALC"
 const UPDATE_CALC = "calculator/UPDATE_CALC"
 const DELETE_CALC = "calculator/DELETE_CALC"
+
+
+const getCalculator = (calculator) =>({
+    type: GET_CALC,
+    calculator
+})
+
+const addCalculator = (calculator) =>({
+    type: ADD_CALC,
+    calculator
+})
+
+const updateCalculator = (calculator) =>({
+    type: UPDATE_CALC,
+    calculator
+})
+
+const deleteCalculator = (calculatorId) =>({
+    type: DELETE_CALC,
+    calculatorId
+})
